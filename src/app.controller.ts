c1965a7ece4d4d6cbc75048164ae5ee4848a7b2b
Connie Leung <connie.leung@diginex.com>
2022-05-20 11:14:26 +0800

    refactor: combine to feature-toggle endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get } from '@nestjs/common'
+import { Controller, Get, Logger } from '@nestjs/common'
 import { FeatureToggleService, FEATURE_FLAGS } from '@/core'
 import { AppService } from './app.service'
 
 @Controller()
 export class AppController {
+  private logger = new Logger(AppController.name)
+
   constructor(private readonly appService: AppService, private featureToggleService: FeatureToggleService) {}
 
   @Get()
@@ -11,19 +13,19 @@ export class AppController {
     return this.appService.getHello()
   }
 
-  @Get('i18n-feature')
-  isTranslationSupported(): string {
-    if (this.featureToggleService.isEnabled(FEATURE_FLAGS.IS_TRANSLATION_ENABLED)) {
-      return 'i18n enabled'
-    }
-    return 'i18n disabled'
-  }
+  @Get('feature-toggle')
+  testFeatureToggle(): { i18nStatus: string; opinionOfVue: string } {
+    const isTranslationEnabled = this.featureToggleService.isEnabled(FEATURE_FLAGS.IS_TRANSLATION_ENABLED)
+    const isVueCool = this.featureToggleService.isEnabled(FEATURE_FLAGS.IS_VUE3_COOL)
+    this.logger.log('info', `isTranslationEnabled: ${isTranslationEnabled}`)
+    this.logger.log('info', `isVueCool: ${isVueCool}`)
 
-  @Get('vue-feature')
-  isVueCool(): string {
-    if (this.featureToggleService.isEnabled(FEATURE_FLAGS.IS_VUE3_COOL)) {
-      return 'Vue3 + Vite is cool'
+    const i18nStatus = isTranslationEnabled ? 'i18n enabled' : 'i18n disabled'
+    const opinionOfVue = isVueCool ? 'Vue3 + Vite is cool' : "Use Vue2 but I won't like it very much"
+
+    return {
+      i18nStatus,
+      opinionOfVue,
     }
-    return 'Use Vue2'
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
 import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
+import * as express from 'express'
+import * as compression from 'compression'
+import * as morgan from 'morgan'
+import * as helmet from 'helmet'
 import { AppModule } from './app.module'
 import { AppConfigService } from './core'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableCors()
+  app.use(helmet())
+  app.use(morgan('combined'))
+  app.use(express.json({ limit: '50mb' }))
+  app.use(express.urlencoded({ limit: '50mb', extended: true }))
+  app.use(compression())
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
   const configService = app.get(AppConfigService)
   await app.listen(configService.portNumber)
